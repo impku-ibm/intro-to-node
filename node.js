c03cc39e30a723e37f6e036bc5c3c67d42f143bc
Pankaj <pankaju713@gmail.com>
2019-11-22 01:20:56 +0530

    updated package.json.procfile,node.js PORT

--- node.js
+++ node.js
@@ -1,6 +1,7 @@
 var http = require('http');
  var url = require('url');
 var fs = require('fs');
+const PORT = process.env.PORT || 5000
 http.createServer(function(req,res){
 	var q = url.parse(req.url,true);
 	var filename="." + q.pathname+".html";
@@ -21,5 +22,5 @@ res.write(data);
 	//res.write(dates);
 	return res.end();
 	});
-}).listen(8080);
+}).listen(PORT);
 console.log("server running on port 8080");
